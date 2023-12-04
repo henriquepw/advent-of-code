ffbbe62cd4df420b41c114c6cff4c3463bd75032
Henrique Miranda <enrque.ns@gmail.com>
2023-12-04 18:22:46 -0300

    refactor: day 2

--- 2023/src/bin/day_2.rs
+++ 2023/src/bin/day_2.rs
@@ -1,71 +1,63 @@
 use std::{cmp, fs};
 
-fn get_dice_value(dice: &str, color: &str) -> u32 {
-    dice.replace(color, "").trim().parse().unwrap()
-}
+fn parse_dice(dice: &str) -> (u32, &str) {
+    let (value, color) = dice.split_once(" ").unwrap();
+    let count: u32 = value.parse().unwrap();
 
-fn check_possible(dice: &str, color: &str) -> bool {
-    let count = get_dice_value(dice, color);
-
-    match color {
-        "blue" => count <= 14,
-        "green" => count <= 13,
-        "red" => count <= 12,
-        _ => true,
-    }
+    (count, color)
 }
 
 fn part_1(input: &String) -> u32 {
-    let mut total = 0;
+    input
+        .split("\n")
+        .enumerate()
+        .map(|(i, line)| {
+            let (_, rounds) = line.split_once(": ").unwrap();
 
-    input.split("\n").for_each(|line| {
-        let (game, rounds) = line.split_once(": ").unwrap();
+            let is_valid = rounds.split("; ").all(|round| {
+                round.split(", ").all(|dice| {
+                    let (count, color) = parse_dice(dice);
+                    match color {
+                        "blue" => count <= 14,
+                        "green" => count <= 13,
+                        "red" => count <= 12,
+                        _ => true,
+                    }
+                })
+            });
 
-        let is_valid = rounds.split("; ").all(|round| {
-            round.split(", ").all(|dice| match dice {
-                s if s.ends_with(" blue") => check_possible(s, "blue"),
-                s if s.ends_with(" green") => check_possible(s, "green"),
-                s if s.ends_with(" red") => check_possible(s, "red"),
-                _ => true,
-            })
-        });
-
-        if is_valid {
-            total += game.replace("Game ", "").parse::<u32>().unwrap();
-        }
-    });
-
-    total
+            match is_valid {
+                true => u32::try_from(i).unwrap() + 1,
+                false => 0,
+            }
+        })
+        .sum()
 }
 
 fn part_2(input: &String) -> u32 {
-    let mut total = 0;
+    input
+        .split("\n")
+        .map(|line| {
+            let (_, rounds) = line.split_once(": ").unwrap();
+            let mut blue = 0;
+            let mut green = 0;
+            let mut red = 0;
 
-    input.split("\n").for_each(|line| {
-        let (_, rounds) = line.split_once(": ").unwrap();
-        let mut blue_value = 0;
-        let mut green_value = 0;
-        let mut red_value = 0;
+            rounds.split("; ").for_each(|round| {
+                round.split(", ").for_each(|dice| {
+                    let (count, color) = parse_dice(dice);
+                    match color {
+                        "blue" => blue = cmp::max(blue, count),
+                        "green" => green = cmp::max(green, count),
+                        "red" => red = cmp::max(red, count),
+                        _ => (),
+                    }
+                })
+            });
 
-        rounds.split("; ").for_each(|round| {
-            round.split(", ").for_each(|dice| match dice {
-                s if s.ends_with("blue") => {
-                    blue_value = cmp::max(blue_value, get_dice_value(s, "blue"));
-                }
-                s if s.ends_with("green") => {
-                    green_value = cmp::max(green_value, get_dice_value(s, "green"));
-                }
-                s if s.ends_with("red") => {
-                    red_value = cmp::max(red_value, get_dice_value(s, "red"));
-                }
-                _ => (),
-            })
-        });
-
-        total += blue_value * green_value * red_value;
-    });
-
-    total
+            blue * green * red
+        })
+        .sum()
 }
 
 fn main() {
---
+++ 2023/src/bin/day_3.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn part_1(_input: &String) -> u32 {
+    let total = 0;
+
+    total
+}
+
+fn part_2(_input: &String) -> u32 {
+    let total = 0;
+
+    total
+}
+
+fn main() {
+    let input = fs::read_to_string("src/inputs/03").expect("input not found");
+
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}

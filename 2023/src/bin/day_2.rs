fc16be8407c026793e5a76c6edea2870f58835d4
Henrique Miranda <enrque.ns@gmail.com>
2023-12-04 14:16:40 -0300

    day 2 - part 2

--- 2023/src/bin/day_2.rs
+++ 2023/src/bin/day_2.rs
@@ -1,7 +1,11 @@
-use std::fs;
+use std::{cmp, fs};
 
-fn check_possible(value: &str, color: &str) -> bool {
-    let count: u32 = value.replace(color, "").trim().parse().unwrap();
+fn get_dice_value(dice: &str, color: &str) -> u32 {
+    dice.replace(color, "").trim().parse().unwrap()
+}
+
+fn check_possible(dice: &str, color: &str) -> bool {
+    let count = get_dice_value(dice, color);
 
     match color {
         "blue" => count <= 14,
@@ -34,13 +38,39 @@ fn part_1(input: &String) -> u32 {
     total
 }
 
-fn part_2() -> u32 {
-    0
+fn part_2(input: &String) -> u32 {
+    let mut total = 0;
+
+    input.split("\n").for_each(|line| {
+        let (_, rounds) = line.split_once(": ").unwrap();
+        let mut blue_value = 0;
+        let mut green_value = 0;
+        let mut red_value = 0;
+
+        rounds.split("; ").for_each(|round| {
+            round.split(", ").for_each(|dice| match dice {
+                s if s.ends_with("blue") => {
+                    blue_value = cmp::max(blue_value, get_dice_value(s, "blue"));
+                }
+                s if s.ends_with("green") => {
+                    green_value = cmp::max(green_value, get_dice_value(s, "green"));
+                }
+                s if s.ends_with("red") => {
+                    red_value = cmp::max(red_value, get_dice_value(s, "red"));
+                }
+                _ => (),
+            })
+        });
+
+        total += blue_value * green_value * red_value;
+    });
+
+    total
 }
 
 fn main() {
     let input = fs::read_to_string("src/inputs/02").expect("input not found");
 
     println!("Part 1: {}", part_1(&input));
-    println!("Part 2: {}", part_2());
+    println!("Part 2: {}", part_2(&input));
 }

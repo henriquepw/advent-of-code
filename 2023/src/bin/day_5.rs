50bfef2418869d54a0e917ab1d0c45d4c9821a1b
Henrique Miranda <enrque.ns@gmail.com>
2023-12-08 23:30:16 -0300

    refactor: day 5 - part 1

--- 2023/src/bin/day_5.rs
+++ 2023/src/bin/day_5.rs
@@ -1,70 +1,100 @@
-use std::{collections::HashMap, fs};
+use std::fs;
 
 struct Map {
-    destination: u64,
-    source: u64,
+    to: u64,
+    from: u64,
     range: u64,
 }
 
-fn get_map_value(maps: &HashMap<String, Vec<Map>>, map_name: &str, from: &u64) -> u64 {
-    let list = maps.get(map_name).unwrap();
-    let mut to = from.clone();
+// struct Seed {
+//     to: u64,
+//     range: u64,
+// }
 
+fn get_map_value(from: u64, list: &Vec<Map>) -> u64 {
     for map in list {
-        if *from >= map.source && *from < map.source + map.range {
-            to = map.destination + (from - map.source);
-            break;
+        if from >= map.from && from < map.from + map.range {
+            return map.to + (from - map.from);
         }
     }
 
-    to
+    from.clone()
 }
 
 fn part_1(input: &String) -> u64 {
-    let mut maps: HashMap<String, Vec<Map>> = HashMap::new();
+    let mut maps: Vec<Vec<Map>> = vec![];
+    let mut chunks = input.trim_end().split("\n\n");
+    let seeds = chunks.next().unwrap().replace("seeds: ", "");
 
-    let lists = input.split_once("\n\n").unwrap();
-    let seeds_str = lists.0.replace("seeds: ", "");
-    let seeds = seeds_str.split(" ");
+    chunks.for_each(|chunk| {
+        let mut map = Vec::new();
 
-    lists.1.split("\n\n").for_each(|chunk| {
-        let mut map_name = String::new();
-        chunk.split("\n").enumerate().for_each(|(i, line)| {
-            if i == 0 {
-                map_name = line.replace(" map:", "");
-                maps.insert(map_name.clone(), Vec::new());
-            } else {
-                let map = maps.get_mut(&map_name).unwrap();
-                let values = line
-                    .split(" ")
-                    .map(|l| l.parse::<u64>().unwrap())
-                    .collect::<Vec<u64>>();
+        chunk.split("\n").skip(1).for_each(|line| {
+            let values = line
+                .split(" ")
+                .map(|l| l.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
 
-                map.push(Map {
-                    destination: values[0],
-                    source: values[1],
-                    range: values[2],
-                });
-            }
+            map.push(Map {
+                to: values[0],
+                from: values[1],
+                range: values[2],
+            });
         });
+
+        maps.push(map);
     });
 
     seeds
-        .map(|s| {
-            let soil = get_map_value(&maps, "seed-to-soil", &s.parse().unwrap());
-            let fertilizer = get_map_value(&maps, "soil-to-fertilizer", &soil);
-            let water = get_map_value(&maps, "fertilizer-to-water", &fertilizer);
-            let light = get_map_value(&maps, "water-to-light", &water);
-            let temperature = get_map_value(&maps, "light-to-temperature", &light);
-            let humidity = get_map_value(&maps, "temperature-to-humidity", &temperature);
-
-            get_map_value(&maps, "humidity-to-location", &humidity)
-        })
+        .split(" ")
+        .map(|s| maps.iter().fold(s.parse().unwrap(), get_map_value))
         .min()
         .unwrap()
 }
 
 fn part_2(_input: &String) -> u64 {
+    // let mut maps: HashMap<String, Vec<Map>> = HashMap::new();
+    // let lists = input.split_once("\n\n").unwrap();
+
+    // let seed_str = lists.0.replace("seeds: ", "");
+    // let items: Vec<&str> = seed_str.split(" ").collect();
+    // let seeds = (1..items.len()).step_by(2).map(|i| Seed {
+    //     to: items[i - 1].parse().unwrap(),
+    //     range: items[i].parse().unwrap(),
+    // });
+
+    // lists.1.split("\n\n").for_each(|chunk| {
+    //     let mut map_name = String::new();
+    //     chunk.split("\n").enumerate().for_each(|(i, line)| {
+    //         if i == 0 {
+    //             map_name = line.replace(" map:", "");
+    //             maps.insert(map_name.clone(), Vec::new());
+    //         } else {
+    //             let map = maps.get_mut(&map_name).unwrap();
+    //             let values = line
+    //                 .split(" ")
+    //                 .map(|l| l.parse::<u64>().unwrap())
+    //                 .collect::<Vec<u64>>();
+
+    //             map.push(Map {
+    //                 to: values[0],
+    //                 from: values[1],
+    //                 range: values[2],
+    //             });
+    //         }
+    //     });
+    // });
+
+    // let seed_to_soil = maps.get("seed-to-soil").unwrap();
+    // let mut my_seed_to_soil: Vec<Map> = vec![];
+
+    // seeds.for_each(|seed| {
+    //     let to = seed.to;
+    //     let from = 0;
+    //     let range = seed.range;
+
+    //     my_seed_to_soil.push(Map { to, from, range })
+    // });
     0
 }
 
